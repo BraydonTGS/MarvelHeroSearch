aa3aed60fb782b596f2a671f0d6680822cfce7e6
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-11-01 11:39:35 -0500

    working on Xunit tests for my api methods

---
+++ MarvelHeroSearch.Tests/MarvelApiClientTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MarvelHeroSearch.Client;
+using MarvelHeroSearch.Models.Hero;
+using Xunit;
+namespace MarvelHeroSearch.Tests
+{
+    public class MarvelApiClientTests
+    {
+        [InlineData("thor", "thor")]
+        public void ShouldReturnCharacter(string name, string expected)
+        {
+            // Arrange //
+            var marvelApi = new MarvelApiClient();
+            // Act //
+            var actual = marvelApi.GetCharacter(name);
+            // Assert //
+            Assert.Equal(expected, actual.data.results[0].name);
+        }
+    }
+}
+
