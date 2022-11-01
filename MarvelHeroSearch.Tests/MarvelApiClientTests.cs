8d34da2a5a53164a04dc9df3830c55a2bd06012b
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-11-01 13:18:53 -0500

    added a unit test for my api get character method and started to implement identity into my application

--- MarvelHeroSearch.Tests/MarvelApiClientTests.cs
+++ MarvelHeroSearch.Tests/MarvelApiClientTests.cs
@@ -6,15 +6,16 @@ namespace MarvelHeroSearch.Tests
 {
     public class MarvelApiClientTests
     {
+        [Theory]
         [InlineData("thor", "thor")]
-        public void ShouldReturnCharacter(string name, string expected)
+        public void ShouldReturnCharacterName(string characterName, string expected)
         {
             // Arrange //
             var marvelApi = new MarvelApiClient();
             // Act //
-            var actual = marvelApi.GetCharacter(name);
+            var actual = marvelApi.GetCharacter(characterName).data.results[0].name;
             // Assert //
-            Assert.Equal(expected, actual.data.results[0].name);
+            Assert.Equal(expected, actual);
         }
     }
 }
