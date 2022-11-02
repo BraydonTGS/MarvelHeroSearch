33daacfa1c96d6020885e508f6eaff6d03b62536
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-11-02 13:56:02 -0500

    created an unit test for the GetCharacterByIdMethod

--- MarvelHeroSearch.Tests/MarvelApiClientTests.cs
+++ MarvelHeroSearch.Tests/MarvelApiClientTests.cs
@@ -7,8 +7,12 @@ namespace MarvelHeroSearch.Tests
 {
     public class MarvelApiClientTests
     {
+        // Test Character By Name Method //
         [Theory]
         [InlineData("Thor")]
+        [InlineData("Scarlet Witch")]
+        [InlineData("Iron Man")]
+        [InlineData("Hulk")]
         public void ShouldReturnCharacterName(string characterName)
         {
             // Arrange //
@@ -19,6 +23,25 @@ namespace MarvelHeroSearch.Tests
             // Assert //
             Assert.NotNull(result);
             Assert.Equal(characterName, result.data.results[0].name);
+        }
+
+
+        // Test Character By Id Method //
+        [Theory]
+        [InlineData("1010716")]
+        [InlineData("1011163")]
+        [InlineData("1009739")]
+        [InlineData("1010780")]
+        public void ShouldReturnCharacterId(string id)
+        {
+            // Arrange //
+            var marvelApi = new MarvelApiClient();
+            // Act //
+            var result = marvelApi.GetCharacterById(id);
+
+            // Assert //
+            Assert.NotNull(result);
+            Assert.Equal(id, result.data.results[0].id.ToString());
 
 
         }
